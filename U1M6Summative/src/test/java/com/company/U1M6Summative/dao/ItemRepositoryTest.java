2de4d2cdbfcb610022ef94e8fbafda18fcbbf945
DevAaron <devaaronlee@gmail.com>
2019-09-12 13:31:11 -0400

    unit test done

--- U1M6Summative/src/test/java/com/company/U1M6Summative/dao/ItemRepositoryTest.java
+++ U1M6Summative/src/test/java/com/company/U1M6Summative/dao/ItemRepositoryTest.java
@@ -1,23 +1,71 @@
 package com.company.U1M6Summative.dao;
 
-import org.junit.After;
+import com.company.U1M6Summative.dao.ItemRepository;
+import com.company.U1M6Summative.dto.Item;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ItemRepositoryTest {
 
+    @Autowired
+    ItemRepository itemRepo;
+
     @Before
     public void setUp() throws Exception {
+        itemRepo.deleteAll();
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void saveGetFindAllDeleteItem() {
+        Item item = new Item();
+        item.setName("Chomske");
+        item.setDescription("It's a Cat!");
+        item.setDailyRate(new BigDecimal("88.99"));
+        itemRepo.save(item);
 
+        Item item1 = new Item();
+        item1.setName("Woofer");
+        item1.setDescription("It's a Doggo.");
+        item1.setDailyRate(new BigDecimal("77.12"));
+        itemRepo.save(item1);
+
+        Item getItem = itemRepo.getOne(item.getId());
+        assertEquals(item.getName(), getItem.getName());
+
+        List<Item> itemList = itemRepo.findAll();
+        assertEquals(2, itemList.size());
+
+        itemRepo.deleteById(item.getId());
+        itemList = itemRepo.findAll();
+        assertEquals(item1.getName(), itemList.get(0).getName());
     }
 
+    @Test
+    public void updateItem() {
+        Item item = new Item();
+        item.setName("Chomske");
+        item.setDescription("It's a Cat!");
+        item.setDailyRate(new BigDecimal("88.99"));
+        itemRepo.save(item);
+
+        item.setName("Woofer");
+        item.setDescription("It's a Doggo.");
+        item.setDailyRate(new BigDecimal("77.12"));
+        itemRepo.save(item);
+
+        Item getItem = itemRepo.getOne(item.getId());
+        assertEquals(item.getName(), getItem.getName());
+    }
 
 }
