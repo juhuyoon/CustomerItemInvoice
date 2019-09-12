3521f6fcc86ddd41ac09aa067e5bbf6523f92606
DevAaron <devaaronlee@gmail.com>
2019-09-11 22:16:47 -0400

    dto

--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/ItemRepository.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/ItemRepository.java
@@ -1,4 +1,9 @@
 package com.company.U1M6Summative.dao;
 
-public interface ItemRepository {
+import com.company.U1M6Summative.dto.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer> {
 }
